363385c95192c620c59add3fb1c1ae3db3bd64d3
kentangkotak <68070888+kentangkotak@users.noreply.github.com>
2025-01-04 10:20:04 +0700

    coba

--- src/pages/admin/master/barang/comp/ListPage.vue
+++ src/pages/admin/master/barang/comp/ListPage.vue
@@ -1,61 +1,67 @@
 <template>
   <div class="column fit">
-
     <div class="col full-height">
       <div class="full-height q-pa-sm">
-        <q-list  class="rounded-borders full-height column">
+        <q-list class="rounded-borders full-height column">
           <div class="col-auto">
             <q-item-label header>
               <div class="row full-width items-center">
-
                 <div class="col-grow">
                   <div class="flex items-center">
-                    <app-input v-model="store.params.q" prepend-icon="search" label="Telusuri" 
-                      style="min-width:250px"
+                    <app-input
+                      v-model="store.params.q"
+                      prepend-icon="search"
+                      label="Telusuri"
+                      style="min-width: 250px"
                       :debounce="300"
-                      @update:model-value="(e)=>{
-                        infiniteScroll.reset();
-                        store.getList()
-                        
-                      }"
-                      />
+                      @update:model-value="
+                        (e) => {
+                          infiniteScroll.reset()
+                          store.getList()
+                        }
+                      "
+                    />
                   </div>
                 </div>
 
                 <div class="col-auto">
                   <app-btn icon="add" tooltip="Tambah Data" color="primary" @click="emits('add')" />
                 </div>
-                  
               </div>
             </q-item-label>
             <q-separator inset="item" />
           </div>
 
-          <div ref="scrollTarget" class="col full-height scroll" >
-            <q-infinite-scroll @load="store.loadMore" 
+          <div ref="scrollTarget" class="col full-height scroll">
+            <q-infinite-scroll
+              @load="store.loadMore"
               ref="infiniteScroll"
               :disable="store?.isError || store?.meta?.next_page_url === null"
-              :scroll-target="scrollTarget" 
+              :scroll-target="scrollTarget"
               :offset="150"
               :initial-index="store.params.page"
-              >
+            >
               <q-intersection
                 v-for="(item, i) in store.items"
                 :key="i"
                 transition="fade"
                 class="example-item"
               >
-                <q-item clickable v-ripple @mouseover="hoveredId=item?.id" @mouseleave="hoveredId=null">
+                <q-item
+                  clickable
+                  v-ripple
+                  @mouseover="hoveredId = item?.id"
+                  @mouseleave="hoveredId = null"
+                >
                   <q-item-section avatar>
                     <q-avatar>
-                      <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+                      <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
                     </q-avatar>
                   </q-item-section>
                   <q-item-section>
                     <q-item-label lines="1">{{ item?.namabarang }}</q-item-label>
                     <q-item-label caption lines="2">
                       <span class="text-weight-bold">-- MERK {{ item?.merk }}</span>
-                      
                     </q-item-label>
                   </q-item-section>
                   <q-item-section v-if="hoveredId === item?.id" side>
@@ -70,8 +76,6 @@
                   </q-item-section>
                 </q-item>
                 <q-separator inset="item" />
-
-                
               </q-intersection>
 
               <template v-slot:loading>
@@ -81,8 +85,6 @@
               </template>
             </q-infinite-scroll>
           </div>
-
-          
         </q-list>
       </div>
       <!-- <div class="column full-height flex-center">
@@ -93,11 +95,11 @@
 </template>
 
 <script setup>
-import { useQuasar } from 'quasar';
-import { humanDate, jamTnpDetik } from 'src/modules/utils';
+import { useQuasar } from 'quasar'
+import { humanDate, jamTnpDetik } from 'src/modules/utils'
 // import { useAdminFormMasterBarangStore } from 'src/stores/admin/master/barang/form';
-import { useAdminMasterBarangStore } from 'src/stores/admin/master/barang/list';
-import {  computed, onBeforeMount, ref } from 'vue';
+import { useAdminMasterBarangStore } from 'src/stores/admin/master/barang/list'
+import { computed, onBeforeMount, ref } from 'vue'
 
 // const search = ref(null)
 const store = useAdminMasterBarangStore()
@@ -124,7 +126,7 @@ const del = (item) => {
   $q.dialog({
     title: 'Peringatan',
     message: 'Apakah Data ini akan dihapus?',
-    cancel: true
+    cancel: true,
     // persistent: true
   })
     .onOk(() => {
@@ -139,20 +141,17 @@ const del = (item) => {
     })
 }
 
- 
- 
 // eslint-disable-next-line no-unused-vars
 const next = computed(() => {
   let page = false
-  
-    if(store.meta?.next_page_url) {
-      page = true
-    }
-  
+
+  if (store.meta?.next_page_url) {
+    page = true
+  }
+
   return page
 })
 
- 
 // function loadMore(index, done) {
 //   store.params.page = index
 //   if (store.params.page === 1) {
@@ -169,12 +168,11 @@ const next = computed(() => {
 //   }
 
 //   console.log('load more',next.value);
-  
+
 // }
 </script>
 <style lang="scss" scoped>
-.example-item{
-  height: 56px
+.example-item {
+  height: 56px;
 }
-  
 </style>
