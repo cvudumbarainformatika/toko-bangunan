e8e40f30ded8466143e070aa083cfb895a11ecb9
kentangkotak <ariezlnd69@gmail.com>
2025-04-25 19:33:09 +0700

    wip : menu order barang, pencarian list order

--- src/pages/admin/transaksi/orderbarang/comp/ListPage.vue
+++ src/pages/admin/transaksi/orderbarang/comp/ListPage.vue
@@ -17,7 +17,7 @@
                       @update:model-value="
                         (e) => {
                           infiniteScroll.reset()
-                          // storeOrderH.getList()
+                          storeOrderH.getList()
                         }
                       "
                     />
@@ -54,9 +54,7 @@
                   @mouseleave="hoveredId = null"
                 >
                   <q-item-section avatar>
-                    <q-avatar>
-                      <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
-                    </q-avatar>
+                    <q-avatar color="teal" text-color="white">OR</q-avatar>
                   </q-item-section>
                   <q-item-section>
                     <q-item-label lines="1">{{ item?.noorder }}</q-item-label>
