0ac6ddcc5e29218846552c27896a417e41833fa3
kentangkotak <ariezlnd69@gmail.com>
2025-06-05 22:23:35 +0700

    wew

--- src/pages/admin/transaksi/penerimaan/IndexPage.vue
+++ src/pages/admin/transaksi/penerimaan/IndexPage.vue
@@ -23,7 +23,6 @@
 <script setup>
 // import { useAdminListTransaksiOrderBarangStore } from 'src/stores/admin/transaksi/orderbarang/list'
 // import { useAdminListTransaksiOrderBarangStore } from 'src/stores/admin/transaksi/orderbarang/list'
-import { data } from 'autoprefixer'
 import { useAdminFormTransaksiPenerimaanBarangStore } from 'src/stores/admin/transaksi/penerimaan/form'
 import { defineAsyncComponent, ref } from 'vue'
 const ListPage = defineAsyncComponent(() => import('./comp/ListPage.vue'))
@@ -33,7 +32,6 @@ const isList = ref(true)
 
 const storepenerimaan = useAdminFormTransaksiPenerimaanBarangStore()
 storepenerimaan.itemPenerimaan = []
-storepenerimaan.form.pembayaran = data?.jenis_pembayaran
 // const soterorder = useAdminListTransaksiOrderBarangStore()
 // const storeorder = useAdminListTransaksiOrderBarangStore()
 // storeorder.getorderanfix()
