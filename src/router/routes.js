eecdb098b64188f53fede10cbbff07343c1517ff
pigartpigaro <vichoirul@gmail.com>
2025-08-18 00:56:11 +0700

    route: laporan

--- src/router/routes.js
+++ src/router/routes.js
@@ -109,6 +109,14 @@ const routes = [
         path: '/admin/laporan/pengeluaran',
         component: () => import('pages/admin/laporan/lap_pengeluaran/IndexPage.vue'),
       },
+      {
+        path: '/admin/laporan/aruskas',
+        component: () => import('pages/admin/laporan/aruskas/IndexPage.vue'),
+      },
+      {
+        path: '/admin/laporan/labarugi',
+        component: () => import('pages/admin/laporan/labarugi/IndexPage.vue'),
+      },
 
       // Settings
       {
